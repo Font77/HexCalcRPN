bf76068c06e1e3b9e34a6cd5339dc8f7e5f83dfe
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2018-02-09 16:54:51 -0500

    Added ButtonRowView to layout, MainActivity.onClick

--- app/src/main/java/com/alloydflanagan/hexcalcrpn/MainActivity.kt
+++ app/src/main/java/com/alloydflanagan/hexcalcrpn/MainActivity.kt
@@ -2,11 +2,26 @@ package com.alloydflanagan.hexcalcrpn
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+/**
+ * Main activity: displays output, current entry, and keys. Handles key clicks.
+ */
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    @SuppressWarnings("unused")
+    var hStack = HexStack()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View) {
+        val fred = v as ButtonRowView
+        fred.textSize = 14f
+        Toast.makeText(this, "got a click on ${fred.clickedText} button", Toast.LENGTH_SHORT).show()
     }
 }
